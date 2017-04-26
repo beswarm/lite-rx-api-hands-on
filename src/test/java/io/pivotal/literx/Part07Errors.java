9e8a424b90d03336db0f89a2d6c0afebc3992b3c
beswarm <beswarm@gmail.com>
2017-04-26 15:50:43 +0800

    finish learning

--- src/test/java/io/pivotal/literx/Part07Errors.java
+++ src/test/java/io/pivotal/literx/Part07Errors.java
@@ -16,15 +16,16 @@
 
 package io.pivotal.literx;
 
-import java.util.function.Function;
-
 import io.pivotal.literx.domain.User;
 import org.junit.Test;
 import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSource;
 import reactor.core.publisher.Hooks;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
+import java.util.function.Function;
 
 /**
  * Learn how to deal with errors.
@@ -37,67 +38,72 @@ public class Part07Errors {
 
 //========================================================================================
 
-	@Test
-	public void monoWithValueInsteadOfError() {
-		Mono<User> mono = betterCallSaulForBogusMono(Mono.error(new IllegalStateException()));
-		StepVerifier.create(mono)
-				.expectNext(User.SAUL)
-				.verifyComplete();
+    @Test
+    public void monoWithValueInsteadOfError() {
+        Mono<User> mono = betterCallSaulForBogusMono(Mono.error(new IllegalStateException()));
+        StepVerifier.create(mono)
+                .expectNext(User.SAUL)
+                .verifyComplete();
 
-		mono = betterCallSaulForBogusMono(Mono.just(User.SKYLER));
-		StepVerifier.create(mono)
-				.expectNext(User.SKYLER)
-				.verifyComplete();
-	}
+        mono = betterCallSaulForBogusMono(Mono.just(User.SKYLER));
+        StepVerifier.create(mono)
+                .expectNext(User.SKYLER)
+                .verifyComplete();
+    }
 
-	// TODO Return a Mono<User> containing User.SAUL when an error occurs in the input Mono, else do not change the input Mono.
-	Mono<User> betterCallSaulForBogusMono(Mono<User> mono) {
-		return null;
-	}
-
-//========================================================================================
-
-	@Test
-	public void fluxWithValueInsteadOfError() {
-		Flux<User> flux = betterCallSaulAndJesseForBogusFlux(Flux.error(new IllegalStateException()));
-		StepVerifier.create(flux)
-				.expectNext(User.SAUL, User.JESSE)
-				.verifyComplete();
-
-		flux = betterCallSaulAndJesseForBogusFlux(Flux.just(User.SKYLER, User.WALTER));
-		StepVerifier.create(flux)
-				.expectNext(User.SKYLER, User.WALTER)
-				.verifyComplete();
-	}
-
-	// TODO Return a Flux<User> containing User.SAUL and User.JESSE when an error occurs in the input Flux, else do not change the input Flux.
-	Flux<User> betterCallSaulAndJesseForBogusFlux(Flux<User> flux) {
-		return null;
-	}
+    // Return a Mono<User> containing User.SAUL when an error occurs in the input Mono, else do not change the input Mono.
+    Mono<User> betterCallSaulForBogusMono(Mono<User> mono) {
+        return FluxSource.wrap(mono).onErrorReturn(IllegalStateException.class, User.SAUL).single();
+    }
 
 //========================================================================================
 
-	@Test
-	public void handleCheckedExceptions() {
-		Flux<User> flux = capitalizeMany(Flux.just(User.SAUL, User.JESSE));
+    @Test
+    public void fluxWithValueInsteadOfError() {
+        Flux<User> flux = betterCallSaulAndJesseForBogusFlux(Flux.error(new IllegalStateException()));
+        StepVerifier.create(flux)
+                .expectNext(User.SAUL, User.JESSE)
+                .verifyComplete();
 
-		StepVerifier.create(flux)
-				.verifyError(GetOutOfHereException.class);
-	}
+        flux = betterCallSaulAndJesseForBogusFlux(Flux.just(User.SKYLER, User.WALTER));
+        StepVerifier.create(flux)
+                .expectNext(User.SKYLER, User.WALTER)
+                .verifyComplete();
+    }
 
-	// TODO Implement a method that capitalize each user of the incoming flux using the capitalizeUser() method and emit an error containing a GetOutOfHereException exception
-	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
-	}
+    // Return a Flux<User> containing User.SAUL and User.JESSE when an error occurs in the input Flux, else do not change the input Flux.
+    Flux<User> betterCallSaulAndJesseForBogusFlux(Flux<User> flux) {
+        return flux.onErrorResumeWith(IllegalStateException.class, e -> Flux.just(User.SAUL, User.JESSE));
+    }
 
-	User capitalizeUser(User user) throws GetOutOfHereException {
-		if (user.equals(User.SAUL)) {
-			throw new GetOutOfHereException();
-		}
-		return new User(user.getUsername(), user.getFirstname(), user.getLastname());
-	}
+//========================================================================================
 
-	private class GetOutOfHereException extends Exception {
-	}
+    @Test
+    public void handleCheckedExceptions() {
+        Flux<User> flux = capitalizeMany(Flux.just(User.SAUL, User.JESSE));
+        StepVerifier.create(flux)
+                .verifyError(GetOutOfHereException.class);
+    }
+
+    // Implement a method that capitalize each user of the incoming flux using the capitalizeUser() method and emit an error containing a GetOutOfHereException exception
+    Flux<User> capitalizeMany(Flux<User> flux) {
+        return flux.map(user -> {
+            try {
+                return this.capitalizeUser(user);
+            } catch (GetOutOfHereException e) {
+                throw Exceptions.propagate(e);
+            }
+        });
+    }
+
+    User capitalizeUser(User user) throws GetOutOfHereException {
+        if (user.equals(User.SAUL)) {
+            throw new GetOutOfHereException();
+        }
+        return new User(user.getUsername(), user.getFirstname(), user.getLastname());
+    }
+
+    private class GetOutOfHereException extends Exception {
+    }
 
 }
--- src/test/java/io/pivotal/literx/Part09Adapt.java
+++ src/test/java/io/pivotal/literx/Part09Adapt.java
@@ -16,9 +16,6 @@
 
 package io.pivotal.literx;
 
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-
 import io.pivotal.literx.domain.User;
 import io.pivotal.literx.repository.ReactiveRepository;
 import io.pivotal.literx.repository.ReactiveUserRepository;
@@ -32,103 +29,104 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.concurrent.CompletableFuture;
+
 /**
  * Learn how to adapt from/to RxJava 2 Observable/Single/Flowable and Java 8+ CompletableFuture.
- *
+ * <p>
  * Mono and Flux already implements Reactive Streams interfaces so they are natively
  * Reactive Streams compliant + there are {@link Mono#from(Publisher)} and {@link Flux#from(Publisher)}
  * factory methods.
- *
+ * <p>
  * For RxJava 2, you should not use Reactor Adapter but only RxJava 2 and Reactor Core.
  *
  * @author Sebastien Deleuze
  */
 public class Part09Adapt {
 
-	ReactiveRepository<User> repository = new ReactiveUserRepository();
+    ReactiveRepository<User> repository = new ReactiveUserRepository();
 
 //========================================================================================
 
-	@Test
-	public void adaptToFlowable() {
-		Flux<User> flux = repository.findAll();
-		Flowable<User> observable = fromFluxToFlowable(flux);
-		StepVerifier.create(fromFlowableToFlux(observable))
-				.expectNext(User.SKYLER, User.JESSE, User.WALTER, User.SAUL)
-				.verifyComplete();
-	}
+    @Test
+    public void adaptToFlowable() {
+        Flux<User> flux = repository.findAll();
+        Flowable<User> observable = fromFluxToFlowable(flux);
+        StepVerifier.create(fromFlowableToFlux(observable))
+                .expectNext(User.SKYLER, User.JESSE, User.WALTER, User.SAUL)
+                .verifyComplete();
+    }
 
-	// TODO Adapt Flux to RxJava Flowable
-	Flowable<User> fromFluxToFlowable(Flux<User> flux) {
-		return null;
-	}
+    // Adapt Flux to RxJava Flowable
+    Flowable<User> fromFluxToFlowable(Flux<User> flux) {
+        return Flowable.fromPublisher(flux);
+    }
 
-	// TODO Adapt RxJava Flowable to Flux
-	Flux<User> fromFlowableToFlux(Flowable<User> flowable) {
-		return null;
-	}
-
-//========================================================================================
-
-	@Test
-	public void adaptToObservable() {
-		Flux<User> flux = repository.findAll();
-		Observable<User> observable = fromFluxToObservable(flux);
-		StepVerifier.create(fromObservableToFlux(observable))
-				.expectNext(User.SKYLER, User.JESSE, User.WALTER, User.SAUL)
-				.verifyComplete();
-	}
-
-	// TODO Adapt Flux to RxJava Observable
-	Observable<User> fromFluxToObservable(Flux<User> flux) {
-		return null;
-	}
-
-	// TODO Adapt RxJava Observable to Flux
-	Flux<User> fromObservableToFlux(Observable<User> observable) {
-		return null;
-	}
+    // Adapt RxJava Flowable to Flux
+    Flux<User> fromFlowableToFlux(Flowable<User> flowable) {
+        return Flux.from(flowable);
+    }
 
 //========================================================================================
 
-	@Test
-	public void adaptToSingle() {
-		Mono<User> mono = repository.findFirst();
-		Single<User> single = fromMonoToSingle(mono);
-		StepVerifier.create(fromSingleToMono(single))
-				.expectNext(User.SKYLER)
-				.verifyComplete();
-	}
+    @Test
+    public void adaptToObservable() {
+        Flux<User> flux = repository.findAll();
+        Observable<User> observable = fromFluxToObservable(flux);
+        StepVerifier.create(fromObservableToFlux(observable))
+                .expectNext(User.SKYLER, User.JESSE, User.WALTER, User.SAUL)
+                .verifyComplete();
+    }
 
-	// TODO Adapt Mono to RxJava Single
-	Single<User> fromMonoToSingle(Mono<User> mono) {
-		return null;
-	}
+    // Adapt Flux to RxJava Observable
+    Observable<User> fromFluxToObservable(Flux<User> flux) {
+        return Observable.fromPublisher(flux);
+    }
 
-	// TODO Adapt RxJava Single to Mono
-	Mono<User> fromSingleToMono(Single<User> single) {
-		return null;
-	}
+    // Adapt RxJava Observable to Flux
+    Flux<User> fromObservableToFlux(Observable<User> observable) {
+        return Flux.from(observable.toFlowable(BackpressureStrategy.DROP));
+    }
 
 //========================================================================================
 
-	@Test
-	public void adaptToCompletableFuture() {
-		Mono<User> mono = repository.findFirst();
-		CompletableFuture<User> future = fromMonoToCompletableFuture(mono);
-		StepVerifier.create(fromCompletableFutureToMono(future))
-				.expectNext(User.SKYLER)
-				.verifyComplete();
-	}
+    @Test
+    public void adaptToSingle() {
+        Mono<User> mono = repository.findFirst();
+        Single<User> single = fromMonoToSingle(mono);
+        StepVerifier.create(fromSingleToMono(single))
+                .expectNext(User.SKYLER)
+                .verifyComplete();
+    }
 
-	// TODO Adapt Mono to Java 8+ CompletableFuture
-	CompletableFuture<User> fromMonoToCompletableFuture(Mono<User> mono) {
-		return null;
-	}
+    // Adapt Mono to RxJava Single
+    Single<User> fromMonoToSingle(Mono<User> mono) {
+        return Single.fromPublisher(mono);
+    }
 
-	// TODO Adapt Java 8+ CompletableFuture to Mono
-	Mono<User> fromCompletableFutureToMono(CompletableFuture<User> future) {
-		return null;
-	}
+    // Adapt RxJava Single to Mono
+    Mono<User> fromSingleToMono(Single<User> single) {
+        return Mono.from(single.toFlowable());
+    }
 
+//========================================================================================
+
+    @Test
+    public void adaptToCompletableFuture() {
+        Mono<User> mono = repository.findFirst();
+        CompletableFuture<User> future = fromMonoToCompletableFuture(mono);
+        StepVerifier.create(fromCompletableFutureToMono(future))
+                .expectNext(User.SKYLER)
+                .verifyComplete();
+    }
+
+    // Adapt Mono to Java 8+ CompletableFuture
+    CompletableFuture<User> fromMonoToCompletableFuture(Mono<User> mono) {
+        return mono.toFuture();
+    }
+
+    // Adapt Java 8+ CompletableFuture to Mono
+    Mono<User> fromCompletableFutureToMono(CompletableFuture<User> future) {
+        return Mono.fromFuture(future);
+    }
 }
